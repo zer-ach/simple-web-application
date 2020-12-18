2913a58ccca4dbbdafb32627d74b6977bbf9a323
zer-ach <zeachraf123@gmail.com>
2020-12-18 18:04:52 +0100

    Initial Commit

---
+++ src/main/java/com/example/simplwebapplication/SimpleController.java
@@ -0,0 +1,13 @@
+package com.example.simplwebapplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "hello everyone";
+    }
+}
---
+++ src/test/java/com/example/simplwebapplication/SimplWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
